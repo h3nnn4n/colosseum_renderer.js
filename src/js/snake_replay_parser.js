197183dcb2d65689ffc0d31a3e9ff9f9bfa5a0a8
h3nnn4n <git@renan.digital>
2022-08-21 20:05:44 -0300

    Really basic snake drawing

--- src/js/snake_replay_parser.js
+++ src/js/snake_replay_parser.js
@@ -19,6 +19,7 @@ renderer.renderFrame = function (frameIndex, renderer) {
   const frame = this.frames[frameIndex];
   const snakes = frame.world_state.snakes;
   const foods = frame.world_state.foods;
+  const agentIds = frame.agent_ids;
 
   const xScale = renderer.width / this.config.grid_width;
   const yScale = renderer.height / this.config.grid_height;
@@ -26,11 +27,30 @@ renderer.renderFrame = function (frameIndex, renderer) {
   const xScaleImage = xScale * 0.5;
   const yScaleImage = yScale * 0.5;
 
-  const imageBackgroundScale = 1.2;
-
   renderer.textSize(32);
   renderer.text(frameIndex, 10, 30);
+  renderer.fill(255, 0, 0);
 
+  // Draw the snakes
+  agentIds.forEach((agentId) => {
+    const snake = snakes[agentId];
+
+    renderer.circle(
+      snake.head_position[0] * xScale - xScale * 0.5,
+      snake.head_position[1] * yScale - yScale * 0.5,
+      100,
+    );
+
+    snake.positions.forEach((position) => {
+      renderer.circle(
+        position[0] * xScale - xScale * 0.5,
+        position[1] * yScale - yScale * 0.5,
+        50,
+      );
+    });
+  });
+
+  // Draw the food
   renderer.fill(255, 0, 0);
   foods.forEach((food) => {
     renderer.image(
