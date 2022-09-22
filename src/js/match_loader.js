02c5c67f9a23a6a449953cbe80456162a76296a3
h3nnn4n <git@renan.digital>
2022-09-22 08:11:21 -0300

    Add hacky-ish code to get player data

--- src/js/match_loader.js
+++ src/js/match_loader.js
@@ -15,14 +15,36 @@ function getMatchReplay(url) {
 function getMatchGame(url_) {
   let url = url_.replace('/replay/', '/');
 
-  console.log(`getting match url from ${url}`);
-
+  console.log(`getting match data from ${url}`);
   let data = jsonGet(url);
   const gameName = data.game.name;
+
+  console.log(data);
+  setPlayerInfo(data, url);
 
   console.log(`game name and id is ${data.game.name} ${data.game.id}`);
 
   return gameName;
+}
+
+function setPlayerInfo(data, baseUrl) {
+  const re = new RegExp('/api/match.*', 'i');
+  const participants = data.participants;
+  window.players = [];
+
+  participants.forEach(function (agentId) {
+    let url = baseUrl.replace(re, `/api/agents/${agentId}`);
+    console.log(`getting player data from ${url}`);
+
+    $.ajax({
+      method: 'GET',
+      url: url,
+    }).done(function (data) {
+      console.log(data);
+
+      window.players.push(data);
+    });
+  });
 }
 
 export { getMatchReplay, getMatchGame };
